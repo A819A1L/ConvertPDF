fe216e0989d409910019214a49cbf2304583480f
Abi <51765260+A819A1L@users.noreply.github.com>
2022-01-07 14:34:45 +0000

    Update ConvertPDFtoXLSX_tabula_v2.py
    The only issue with using tkinter is that the window stays open permanently. I haven't managed to find a solution to close it at the end of the program without getting an error. Also need to work out how to open file location and type a file name without having to hard code it in.

--- ConvertPDFtoXLSX_tabula_v2.py
+++ ConvertPDFtoXLSX_tabula_v2.py
@@ -1,62 +1,88 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-pip install tabula-py
-
-
-# In[2]:
-
-
-# Import modules 
+# Version 1
 import tabula
-from PyPDF2 import PdfFileReader
+import pandas as pd
 
+pdf_name = "/Users/adavies/Gardners/DocCentral/Analysis/07 General Operations/03 Transport/04 FY2022/TUFFNELLS/TuffInvoice.pdf"
+ExcelOutputPath = "/Users/adavies/Gardners/DocCentral/Analysis/07 General Operations/03 Transport/04 FY2022/TUFFNELLS/TuffInvoice.xlsx" # to save each page as individual file
 
-# In[3]:
+FinalFileName = "/Users/adavies/Gardners/DocCentral/Analysis/07 General Operations/03 Transport/04 FY2022/TUFFNELLS/TuffInvoice.xlsx"
 
+dfAllPages = tabula.read_pdf(pdf_name, pages='all')
 
-# Get attribute of PDF file
-pdf_path = "/Users/adavies/Gardners/DocCentral/Analysis/7. General Operations/3. Transport/4. Invoice Data FY 21/Tuffnells/43INV-03410207.pdf"
+dfMergedData = pd.DataFrame()
 
-def extract_information(pdf_path):
-    with open(pdf_path, 'rb') as f:
-        pdf = PdfFileReader(f)
-        information = pdf.getDocumentInfo()
-        number_of_pages = pdf.getNumPages()
+counter = 0
 
-    txt = f"""
-    Information about {pdf_path}: 
-    Number of pages: {number_of_pages}
-    """
+for singlePage in dfAllPages:
+   
+    dfSinglePage = tabula.read_pdf(pdf_name, pages='all')[counter]
 
-    print(txt)
-    return information
+    ExcelFileName = ExcelOutputPath + str(counter) + '.xlsx' # to save each page as individual file
+    print (counter)
 
-if __name__ == '__main__':
-    path = pdf_path
-    extract_information(path)
+    # EXPORT AN INDIVIDUAL PAGE
+    dfSinglePage.to_excel(ExcelFileName) # to save each page as individual file
 
+    #MERGE THE SINGLE PAGE INTO A COMBINED DATAFRAME
+    dfMergedData = dfMergedData.append(dfSinglePage,ignore_index=True)
 
-# In[4]:
-
-
-# Read PDF file
-AllTablesInPDF = tabula.read_pdf(pdf_path, pages = "all")
-
-Counter = 1   
-# Convert into excel file
-for singleTable in AllTablesInPDF:
+    #EXPORT THE COMBINED DATAFRAME
+    dfMergedData.to_excel(FinalFileName)
     
-    singleTable.to_excel('/Users/adavies/Gardners/DocCentral/Analysis/7. General Operations/3. Transport/4. Invoice Data FY 21/Tuffnells/43TuffInvoicev8.xlsx',sheet_name=str(Counter))
+    counter = counter + 1
+ 
+
+# Versio 2
+# This code takes empty rows out before exporting to excel
+import tabula
+import pandas as pd
+pdf_name = "/Users/adavies/Gardners/DocCentral/Analysis/07 General Operations/03 Transport/04 FY2022/TUFFNELLS/TuffInvoice.pdf"
+FinalFileName = "/Users/adavies/Gardners/DocCentral/Analysis/07 General Operations/03 Transport/04 FY2022/TUFFNELLS/TuffInvoice.xlsx"
+dfAllPages = tabula.read_pdf(pdf_name, pages='all')
+dfMergedData = pd.DataFrame()
+dfCleanedData = pd.DataFrame()
+counter = 0
+for singlePage in dfAllPages:
+   
+    dfSinglePage = tabula.read_pdf(pdf_name, pages='all')[counter]
     
-    Counter = Counter + 1
+    print ("Page:" + str(counter + 1))
+    # Merge the single page into a combined dataframe
+    
+    dfMergedData = dfMergedData.append(dfSinglePage,ignore_index=True)
+    counter = counter + 1
+# Remove any rows where the cell under "account" heading is blank
+dfCleanedData = dfMergedData.dropna(subset=["Account"])
+ 
+# Export the clean dataframe
+dfCleanedData.to_excel(FinalFileName)
 
 
-# In[ ]:
-
+# Version 3
+# This code brings up a dialog box to ask you to pick a file to convert so you don't have to hard code the pdf file into your script
+import tabula
+import pandas as pd
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+pdf_name = askopenfilename()
+FinalFileName = "/Users/adavies/Gardners/DocCentral/Analysis/07 General Operations/03 Transport/04 FY2022/TUFFNELLS/TuffInvoice.xlsx" # Assign name
+dfAllPages = tabula.read_pdf(pdf_name, pages='all')
+dfMergedData = pd.DataFrame()
+dfCleanedData = pd.DataFrame()
+counter = 0
+for singlePage in dfAllPages:
+   
+    dfSinglePage = tabula.read_pdf(pdf_name, pages='all')[counter]
+    
+    print ("Page:" + str(counter + 1))
+    # Merge the single page into a combined dataframe
+    dfMergedData = dfMergedData.append(dfSinglePage,ignore_index=True)
+    counter = counter + 1
+# Remove any rows where the cell under "account" heading is blank     
+dfCleanedData = dfMergedData.dropna(subset=["Account"])
+ 
+# Export the clean dataframe
+dfCleanedData.to_excel(FinalFileName)
 
 
 
